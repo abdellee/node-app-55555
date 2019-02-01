6feed94adf390c5d5988cb5a8b180b2bb3ac362b
Magnalane Coders <shamroukh62@hotmail.com>
2019-01-31 21:45:43 -0600

    adding index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+//importing node framework
+var express = require('express');
+ 
+var app = express();
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+ res.send('hello world from 5555');
+});
+//listen to port 5555 by default
+app.listen(process.env.PORT || 5555);
+ 
+module.exports = app;
